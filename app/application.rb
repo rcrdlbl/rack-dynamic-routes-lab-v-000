bdc3a67d8ee49d77618a42b693cf8b7ac5842b21
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-10 23:20:44 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,4 +1,21 @@
 class Application
   @@items = []
-  
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+
+      item_name = req.path.split(/items/).last
+      item = @@items.find{|i| i.name == item.name}
+
+      if @@items.find{|i| i.name == item.name}
+        resp.write item.price
+      else
+        resp.status = 400
+        resp.write "Item not found"
+    else
+      
+      
 end
